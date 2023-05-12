293c593d9b3b7a8204d4a65ca32eb860cab8acba
FACULTAD CIENCIAS ADMINISTRATIVAS <fca.laboratorio@uce.edu.ec>
2023-05-12 17:38:19 -0500

    Taller 2

---
+++ src/main/java/com/example/demo/PaU1P4JpApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaU1P4JpApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaU1P4JpApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+	System.out.println("Mi primer programa");	
+	}
+
+}
