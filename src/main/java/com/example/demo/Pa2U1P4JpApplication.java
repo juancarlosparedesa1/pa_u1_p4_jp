73f1b60f311e37368d5a7d6a5a2bf39d4425957f
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-05-12 18:44:18 -0500

    Taller 1

---
+++ src/main/java/com/example/demo/Pa2U1P4JpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P4JpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P4JpApplication.class, args);
+	}
+
+}
