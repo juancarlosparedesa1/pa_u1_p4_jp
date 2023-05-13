628ded1c972bdc3ae42dcf9d237bef1dda83fabe
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-05-12 19:34:27 -0500

    comentario luego del almuerzo

--- src/main/java/com/example/demo/PaU1P4JpApplication.java
+++ src/main/java/com/example/demo/PaU1P4JpApplication.java
@@ -14,13 +14,12 @@ public class PaU1P4JpApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-	System.out.println("Mi primer programa");
-	/*
-	 * Spring framework es un framework  que permite la construcion/desarrollo
-	 * de aplicaciones empresariales basadas en spring
-	 * */
-	
-	
+		System.out.println("Mi primer programa");
+		/*
+		 * Spring framework es un framework que permite la construcion/desarrollo de
+		 * aplicaciones empresariales basadas en spring comentario luego del almuerzo
+		 */
+
 	}
 
 }
