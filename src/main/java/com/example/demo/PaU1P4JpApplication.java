31f0dfe8a7e07540da269020f114dfe22b440643
juancarlosparedesa1 <jcparedesa1@uce.edu.ec>
2023-05-16 19:39:01 -0500

    taller5

--- src/main/java/com/example/demo/PaU1P4JpApplication.java
+++ src/main/java/com/example/demo/PaU1P4JpApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PaU1P4JpApplication implements CommandLineRunner {
+
+	@Autowired
+	private Profesor profesor1;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaU1P4JpApplication.class, args);
@@ -23,13 +27,32 @@ public class PaU1P4JpApplication implements CommandLineRunner {
 		 */
 
 		// commit subida de prueba antes del taller
+		System.out.println("Mi primer proyecto");
 
 		Profesor profesor = new Profesor();
+
+		profesor.setNombre("juan Carlos");
 		profesor.setApellido("Paredes");
 		profesor.setCedula("1726897299");
 		profesor.setFechaNacimiento(LocalDateTime.now());
-		profesor.setNombre("juan Carlos");
+
+		
 		System.out.println(profesor);
+		profesor.setApellido("angulo");
+		System.out.println(profesor.getApellido());
+
+		System.out.println(profesor);
+
+		// profesor1 = new Profesor();
+		// esto da error?porque
+		System.out.println(profesor1);
+		profesor1.setApellido("benitez");// porque imprime null=)(/
+		profesor1.setApellido("");
+		System.out.println(profesor1.getApellido());
+
+		MatriculaCalculo calculo = new MatriculaCalculo();
+		calculo.realizarMatricula("1");
+
 	}
 
 }
--- src/main/java/com/example/demo/Profesor.java
+++ src/main/java/com/example/demo/Profesor.java
@@ -2,11 +2,15 @@ package com.example.demo;
 
 import java.time.LocalDateTime;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Profesor {
 	private String nombre;
 	private String apellido;
 	private LocalDateTime fechaNacimiento;
 	private String cedula;
+	
 
 	// set y get
 	public String getNombre() {
