ae04bd3a3afd3708b2442de821effaf23f660212
FACULTAD CIENCIAS ADMINISTRATIVAS <jcapredesa1@uce.edu.ec>
2023-06-02 13:39:25 -0500

    spring01_uv

---
+++ src/main/java/com/example/demo/banco/service/TransferenciaComision.java
@@ -0,0 +1,8 @@
+package com.example.demo.banco.service;
+
+import java.math.BigDecimal;
+
+public interface TransferenciaComision {
+	public BigDecimal calcularMontoComision(BigDecimal monto);
+
+}
---
+++ src/main/java/com/example/demo/banco/service/TransferenciaComisionNacionalImpl.java
@@ -0,0 +1,19 @@
+package com.example.demo.banco.service;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Service;
+
+@Service("Nacional")
+public class TransferenciaComisionNacionalImpl implements TransferenciaComision {
+
+	@Override
+	public BigDecimal calcularMontoComision(BigDecimal monto) {
+		// si es nacional se le suma una comision del 5% del monto
+		// esto se le resta a la cuenta origen
+		BigDecimal comision = monto.multiply(new BigDecimal(0.05));
+		return monto.add(comision);
+
+	}
+
+}
